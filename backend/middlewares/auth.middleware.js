55b3e367cce0afc0da59aacc7268bd0bfab5c7b1
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-08-05 02:13:21 +0530

    refresh token login built using smart middleware pattern

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,16 +3,16 @@ const UserModel = require("../models/User.model");
 require("dotenv").config();
 
 const authenticateuser = async (req, res, next) => {
+  //get token from cookie
+  const token = req.cookies.jwt;
+  const refreshToken = req.cookies.refreshjwt;
+  const jwt_secret = process.env.JWT_SECRET;
+  const REFRESH_SECRET = process.env.REFRESHTOKEN_SECRET;
+  if (!token) {
+    res.status(401).json({ message: "not authorized" });
+    return;
+  }
   try {
-    //get token from cookie
-    const token = req.cookies.jwt;
-
-    if (!token) {
-      res.status(401).json({ message: "not authorized" });
-      return;
-    }
-
-    const jwt_secret = process.env.JWT_SECRET;
     //verify the token
     const decoded = jwt.verify(token, jwt_secret);
     //find user from db by the userid in payloads of token
@@ -22,14 +22,43 @@ const authenticateuser = async (req, res, next) => {
         .status(401)
         .json({ message: "Not authorized, user not found" });
     }
-    // Attach the authenticated user object to the request object this allows subsequent request to use this 
+    // Attach the authenticated user object to the request object this allows subsequent request to use this
     req.user = user;
     next();
   } catch (err) {
-    res.status(500).json({ message: "internal server error ", err });
-    return;
+    if (err.name !== "TokenExpiredError") {
+      //error anything other than expiry, the token is bad
+      return res.status(403).json({ message: "Forbidden: Invalid token" });
+    }
+
+    //token is  EXPIRED. Now we refresh
+    if (!refreshToken) {
+      return res
+        .status(401)
+        .json({ message: "Access token expired, no refresh token provided" });
+    }
+    try {
+      //verify the token
+      const decoded = jwt.verify(refreshToken, REFRESH_SECRET);
+      //find user from db by the userid in payloads of token
+      const user = await UserModel.findById(
+        decoded.userid,
+        "_id Email Username"
+      );
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Not authorized, user not found" });
+      }
+      // Attach the authenticated user object to the request object this allows subsequent request to use this
+     req.user = user;
+      next();
+    } catch (err) {
+      res.status(500).json({ message: "internal server error ", err });
+      return;
+    }
   }
 };
 
 module.exports = authenticateuser;
-//authentication middleware 
+//authentication middleware
--- backend/utils/jwt.utility.js
+++ backend/utils/jwt.utility.js
@@ -1,20 +1,36 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
+const refresh_secret = process.env.REFRESHTOKEN_SECRET;
 const jwt_secret = process.env.JWT_SECRET;
-//generate jwt token 
+//generate jwt token
 const generatejwt = (res, userid) => {
   //sign token with the secret key and put in userid in payloads
-  const token = jwt.sign({ userid }, jwt_secret, { expiresIn: "1h" });
-//send the token via cookie
+  const token = jwt.sign({ userid }, jwt_secret, { expiresIn: "1m" });
+  //send the token via cookie
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     samesite: "strict",
-    maxAge: 3600000,
+    maxAge:   60 * 1000,
     path: "/",
   });
 };
+//add a refresh token after the acess token
+const refreshjwt = (res, userid) => {
+  //sign token with the secret key and put in userid in payloads
+  const refreshtoken = jwt.sign({ userid }, refresh_secret, {
+    expiresIn: "7d",
+  });
+  //send the token via cookie
+  res.cookie("refreshjwt", refreshtoken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    samesite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
+  });
+};
+
 //clear the token from cookie
 const clearjwt = (res) => {
   res.cookie("jwt", "", {
@@ -24,6 +40,13 @@ const clearjwt = (res) => {
     expires: new Date(0),
     path: "/",
   });
+  res.cookie("refreshjwt", "", {
+    httpOnly: true,
+    samesite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    expires: new Date(0),
+    path: "/",
+  });
 };
 
-module.exports = { generatejwt, clearjwt };
+module.exports = { generatejwt, clearjwt, refreshjwt };
