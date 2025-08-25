574c1d8990b9637cfbbe28ca7c81e764e9b128aa
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-08-26 01:29:48 +0530

    zod middleware error fixed

--- backend/middlewares/validate.middleware.js
+++ backend/middlewares/validate.middleware.js
@@ -1,26 +1,10 @@
-const { ZodError } = require("zod");
-
-const validate = (schema) => (req, res, next) => {
+const validate = (schema) => async (req, res, next) => {
   try {
-    if (schema?.body) schema.body.parse(req.body);
-    if (schema?.query) schema.query.parse(req.query);
-    if (schema?.params) schema.params.parse(req.params);
-    next();
-  } catch (error) {
-    if (error instanceof ZodError) {
-      const formatted =
-        error.errors?.map((err) => ({
-          path: err.path.join("."),
-          message: err.message,
-        })) || [];
+    await schema.parseAsync(req.body);
 
-      return res.status(400).json({
-        type: "zod",
-        message: "Validation Error",
-        errors: formatted,
-      });
-    }
-    return res.status(500).json({ message: "Internal server error" });
+    return next();
+  } catch (error) {
+    return res.status(400).json(error);
   }
 };
 
