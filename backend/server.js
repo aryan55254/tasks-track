fe7530987b4ed272e3ff911a479c9b6dd9e95c5f
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-08-31 00:48:51 +0530

    frontend backend connected

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,12 @@ const taskroutes = require("./routes/Taks.routes");
 const app = express();
 
 //middleware
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://tasks-track-three.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieparser());
 
