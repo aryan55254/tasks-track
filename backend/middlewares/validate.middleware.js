d9c31fc2c96259cb979d85881c4a76d661410ccc
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-09-06 20:32:46 +0530

    updated zod schma and middleware for better error handling

--- backend/middlewares/validate.middleware.js
+++ backend/middlewares/validate.middleware.js
@@ -1,10 +1,27 @@
+const { ZodError } = require("zod");
+
 const validate = (schema) => async (req, res, next) => {
   try {
-    await schema.parseAsync(req.body);
-
+    await schema.parseAsync({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
     return next();
   } catch (error) {
-    return res.status(400).json(error);
+    if (error instanceof ZodError) {
+      const formattedErrors = {};
+      error.issues.forEach((issue) => {
+        const fieldName = issue.path[issue.path.length - 1];
+        formattedErrors[fieldName] = issue.message;
+      });
+
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: formattedErrors,
+      });
+    }
+    next(error);
   }
 };
 
--- backend/validation_schema/auth.validation.js
+++ backend/validation_schema/auth.validation.js
@@ -1,40 +1,44 @@
 const { z } = require("zod");
 
-const registerschema = z.object({
-  Username: z
-    .string({
-      required_error: "Username Is required",
-    })
-    .min(3, { message: "Username must be at least 3 characters long" }),
+const registerSchema = z.object({
+  body: z.object({
+    Username: z
+      .string({
+        required_error: "Username Is required",
+      })
+      .min(3, { message: "Username must be at least 3 characters long" }),
 
-  Email: z
-    .string({
-      required_error: "Email is required",
-    })
-    .email({ message: "Invalid email adress" }),
+    Email: z
+      .string({
+        required_error: "Email is required",
+      })
+      .email({ message: "Invalid email adress" }),
 
-  Password: z
-    .string({
-      required_error: "password is required",
-    })
-    .min(6, { message: "password must be at least 6 charcaters long" }),
+    Password: z
+      .string({
+        required_error: "password is required",
+      })
+      .min(6, { message: "password must be at least 6 charcaters long" }),
+  }),
 });
 
 const loginSchema = z.object({
-  Email: z
-    .string({
-      required_error: "Email is required",
-    })
-    .email({ message: "Invalid email address" }),
+  body: z.object({
+    Email: z
+      .string({
+        required_error: "Email is required",
+      })
+      .email({ message: "Invalid email address" }),
 
-  Password: z
-    .string({
-      required_error: "Password is required",
-    })
-    .nonempty("Password cannot be empty"),
+    Password: z
+      .string({
+        required_error: "Password is required",
+      })
+      .nonempty("Password cannot be empty"),
+  }),
 });
 
 module.exports = {
-  registerschema,
+  registerSchema,
   loginSchema,
 };
